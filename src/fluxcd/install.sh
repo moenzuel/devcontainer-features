88c98939ccf07e9a2d0491d06507f987cc4b0fc0
Bryan Clark <me@moenzuel.com>
2022-11-20 04:53:34 +0000

    [fluxcd] Update zsh for remote docker user home

--- src/fluxcd/install.sh
+++ src/fluxcd/install.sh
@@ -4,14 +4,14 @@ set -e
 FLUX_VERSION=${VERSION:-"latest"}
 
 # do install
-curl -s https://raw.githubusercontent.com/fluxcd/flux2/main/install/flux.sh | sudo bash
+curl -s https://raw.githubusercontent.com/fluxcd/flux2/main/install/flux.sh | bash
 
 # bash completion
 flux completion bash > /etc/bash_completion.d/fluxcd
 
 # zsh completion
-if [ -e "${USERHOME}}/.oh-my-zsh" ]; then
-    mkdir -p "${USERHOME}/.oh-my-zsh/completions"
-    flux completion zsh > "${USERHOME}/.oh-my-zsh/completions/_fluxcd"
-    chown -R "${USERNAME}" "${USERHOME}/.oh-my-zsh"
+if [ -e "${_REMOTE_USER_HOME}/.oh-my-zsh" ]; then
+    mkdir -p "${_REMOTE_USER_HOME}/.oh-my-zsh/completions"
+    flux completion zsh > "${_REMOTE_USER_HOME}/.oh-my-zsh/completions/_fluxcd"
+    chown -R "${_REMOTE_USER_HOME}" "${_REMOTE_USER_HOME}/.oh-my-zsh"
 fi
